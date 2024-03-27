98b4b77c6fec477764b2f4f21befe2e0573a14d8
David Schüler <92929152+davSchueler@users.noreply.github.com>
2024-03-27 14:17:06 +0100

    Add files via upload

--- tfidf_vertorizer.py
+++ tfidf_vertorizer.py
@@ -4,35 +4,46 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+def hasNumbers(string):
+    hasDigit = False
+    for char in string:
+        if(char.isdigit()):
+            hasDigit = True
+    return hasDigit
 
-with open('train_raw_texts.txt', 'r', encoding="utf-8") as file:
+def listToString(list_of_strings):
+    output_string = ""
+    for string in list_of_strings:
+        output_string += string + " "
+    return output_string
+
+
+with open('./train_raw_texts.txt', 'r', encoding="utf-8") as file:
     lines = file.readlines()
-
 corpus= [line.strip() for line in lines]
 file.close()
 
 text = corpus[0].lower()
+text_split = text.split(" ")
+
+filtered_text_split = []
+for word in text_split:
+    if not("'" in word or "-" in word or "’" in word or hasNumbers(word)):
+        filtered_text_split.append(word)
+
+filtered_text_string = listToString(filtered_text_split)
 
 stop_words = set(stopwords.words('english') + list(string.punctuation) + ["``","''","’"])
+tokens = word_tokenize(filtered_text_string)
 
-tokens = word_tokenize(text)
-#print(tokens)
+filtered_tokens = []
+for token in tokens:
+    if not (token in stop_words):
+        filtered_tokens.append(token)
 
-token_set = set(tokens)
+filtered_tokens_set = list(set(filtered_tokens))
+filtered_tokens_set.sort()
 
-filtered_token_set = []
+preprocessed_string = listToString(filtered_tokens_set)
 
-for token in token_set:
-    if not (token in stop_words) and not token.isdigit():
-        filtered_token_set.append(token)
-
-filtered_token_set.sort()
-
-filtered_token_string = ""
-
-for token in filtered_token_set:
-    filtered_token_string += token + " "
-
-print(filtered_token_string)
-
-
+print(preprocessed_string)
